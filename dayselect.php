218dae81d8717b4e4f1730d59f86b44e26f62ad2
cardmaster0 <144099094+cardmaster0@users.noreply.github.com>
2025-12-19 11:43:48 +0900

    Update dayselect.php

--- dayselect.php
+++ dayselect.php
@@ -3,36 +3,22 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>教室予約</title>
-
-    <link rel="stylesheet" href="./style.css" />
-    <script src="./script.js" defer></script>
+    <title>日時選択</title>
   </head>
 
   <body>
-    <form action="reserve.php" method="get">
+    <form action="selectroom.php" method="get">
       <div>
         <label>日付</label>
-        <input type="date" id="inputDate" name="date" required />
+        <input type="date" name="date" required />
       </div>
 
       <div>
         <label>時間</label>
-        <input type="time" id="inputTime" name="time" required />
+        <input type="time" name="time" required />
       </div>
 
-      <div>
-        <label>教室</label>
-        <select id="room" name="room" required>
-          <option value="" selected disabled>教室を選択</option>
-          <option value="A101">A101</option>
-          <option value="A102">A102</option>
-          <option value="B201">B201</option>
-          <option value="B202">B202</option>
-        </select>
-      </div>
-
-      <button type="submit">予約へ進む</button>
+      <button type="submit">教室選択へ</button>
     </form>
   </body>
 </html>
