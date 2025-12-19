c598463e14c7f714ed454d6e959b9fb26853a3e2
cardmaster0 <144099094+cardmaster0@users.noreply.github.com>
2025-12-19 12:14:04 +0900

    Update dayselect_3.php

--- dayselect_3.php
+++ dayselect_3.php
@@ -1,24 +1,15 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>日時選択</title>
-  </head>
-
-  <body>
-    <form action="selectroom.php" method="get">
-      <div>
-        <label>日付</label>
-        <input type="date" name="date" required />
-      </div>
-
-      <div>
-        <label>時間</label>
-        <input type="time" name="time" required />
-      </div>
-
-      <button type="submit">教室選択へ</button>
-    </form>
-  </body>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>日付選択</title>
+</head>
+<body>
+  <form action="selectroom.php" method="get">
+    <label>日付</label>
+    <input type="date" name="date" required />
+    <button type="submit">教室選択へ</button>
+  </form>
+</body>
 </html>
