5be87514d17e7f9946b678958cb99b91c241e5f1
cardmaster0 <144099094+cardmaster0@users.noreply.github.com>
2026-01-13 15:40:21 +0900

    Hash password before updating member data

--- member.php
+++ member.php
@@ -13,17 +13,18 @@ $pdo = db_connect();
 
 // セッション変数から受け取ります。
 $id  = $_SESSION['id'];
-
+passHash = password_hash($pass,PASSWORD_DEFAULT);
 try {
   $pdo->beginTransaction();
   $sql = "UPDATE  member
             SET 
             pass  = :pass,
-              name = :name,
+              name = :name
           WHERE id = :id";
   $stmh = $pdo->prepare($sql);
   $stmh->bindValue(':pass',  $_POST['pass'],  PDO::PARAM_STR );
   $stmh->bindValue(':name', $_POST['name'], PDO::PARAM_STR );
+    $stmh->bindValue(':id', $_POST['id'], PDO::PARAM_STR );
   $stmh->execute();
   $pdo->commit();
   print "データを" . $stmh->rowCount() . "件、更新しました。<br>";
