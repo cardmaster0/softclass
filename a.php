5915b46ea02eb6a8910f87cd6a2f5d80d735892e
cardmaster0 <144099094+cardmaster0@users.noreply.github.com>
2026-01-13 15:25:59 +0900

    Add files via upload

---
+++ a.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title>メンバー管理</title>
+</head>
+<body>
+<?php
+require_once("MYDB.php");
+$pdo = db_connect();
+
+// セッション変数から受け取ります。
+$id  = $_SESSION['id'];
+
+try {
+  $pdo->beginTransaction();
+  $sql = "UPDATE  member
+            SET 
+            pass  = :pass,
+              name = :name,
+          WHERE id = :id";
+  $stmh = $pdo->prepare($sql);
+  $stmh->bindValue(':pass',  $_POST['pass'],  PDO::PARAM_STR );
+  $stmh->bindValue(':name', $_POST['name'], PDO::PARAM_STR );
+  $stmh->execute();
+  $pdo->commit();
+  print "データを" . $stmh->rowCount() . "件、更新しました。<br>";
+  
+} catch (PDOException $Exception) {
+  $pdo->rollBack();
+  print "エラー：" . $Exception->getMessage();
+}
+
+// セッション変数を全て解除する
+$_SESSION = array();
+
+// 最終的に、セッションを破壊する
+session_destroy();
+
+
+?>
+</body>
+</html>
