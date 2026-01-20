0dd8cda3ccd0cc4a0241cf967740fd72e7267a80
saku-46 <saku.semba.s@gmail.com>
2026-01-20 16:36:50 +0900

    Add files via upload

--- login_form.php
+++ login_form.php
@@ -1,16 +1,18 @@
-<h1>ログインページ</h1>
-<form action="login.php" method="post">
-<div>
-    <label>
-        AXIAアカウント：
-        <input type="text" name="id" required>
-    </label>
-</div>
-<div>
-    <label>
-        パスワード：
-        <input type="password" name="pass" required>
-    </label>
-</div>
-<input type="submit" value="ログイン">
-</form>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ログインページ</title>
+    </head>
+    <body>
+        <h1>ログインページ</h1>
+        <form action="login.php" method="post">
+            <div>
+                <label>AXIAアカウント：<input type="text" name="id" required></label>
+            </div>
+            <div>
+                <label>パスワード：<input type="password" name="pass" required></label>
+            </div>
+            <input type="submit" value="ログイン">
+        </form>
+    </body>
+</html>
